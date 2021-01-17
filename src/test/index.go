132f32c5e3db786aae923dc13f124b9029a1d9f3
littlestarsx <loveshao1113@126.com>
2021-01-17 21:18:18 +0800

    语法

--- src/test/index.go
+++ src/test/index.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 //func main() {
 //	fmt.Println("Hello World");
 //}
@@ -55,9 +53,73 @@ import "fmt"
 //}
 
 
-var c, python, java bool
+//var c, python, java bool
+//
+//func main() {
+//	var i int
+//	fmt.Println(i, c, python, java)
+//}
 
-func main() {
-	var i int
-	fmt.Println(i, c, python, java)
-}
+
+//var i, j int = 1, 2
+//
+//func main() {
+//	var c, python, java = true, false, "no!"
+//	fmt.Println(i, j, c, python, java)
+//}
+
+
+//func main() {
+//	var i, j int = 1, 2
+//	k := 3
+//	c, python, java := true, false, "no!"
+//	fmt.Println(i, j, k, c, python, java)
+//}
+
+
+//var (
+//	ToBe bool = false
+//	MaxInt uint64 = 1<<64 - 1
+//	z complex128 = cmplx.Sqrt(-5 + 12i)
+//)
+//
+//func main() {
+//	fmt.Printf("Type: %T Value: %v\n", ToBe, ToBe)
+//	fmt.Printf("Type: %T Value: %v\n", MaxInt, MaxInt)
+//	fmt.Printf("Type: %T Value: %v\n", z, z)
+//}
+
+
+//func main() {
+//	var i int
+//	var f float64
+//	var b bool
+//	var s string
+//	fmt.Printf("%v %v %v %q\n", i, f, b, s)
+//}
+
+
+//func main() {
+//	var x, y int = 3, 4
+//	var f float64 = math.Sqrt(float64(x*x + y*y))
+//	var z uint = uint(f)
+//	fmt.Println(x, y, z)
+//}
+
+
+//func main() {
+//	v := 42 // 修改这里！
+//	fmt.Printf("v is of type %T\n", v)
+//}
+
+
+//const Pi = 3.14
+//
+//func main() {
+//	const World = "世界"
+//	fmt.Println("Hello", World)
+//	fmt.Println("Happy", Pi, "Day")
+//
+//	const Truth = true
+//	fmt.Println("Go rules?", Truth)
+//}
