43e7ab5ef680ae0cc66cafcb8f681583fe7f5562
littlestarsx <loveshao1113@126.com>
2021-02-01 22:54:25 +0800

    语法

--- src/test/index.go
+++ src/test/index.go
@@ -1,3 +1,6 @@
+/**
+Go 指南
+ */
 package main
 
 //func main() {
@@ -5,16 +8,24 @@ package main
 //}
 
 
-//func main(){
+//包
+//func main() {
 //	fmt.Println("My favorite number is,", rand.Intn(10))
 //}
 
 
+//导入
+//func main() {
+//	fmt.Printf("Now you have %g problems.\n", math.Sqrt(7))//开平方
+//}
+
+
+//导出名
 //func main() {
 //	fmt.Println(math.Pi)
 //}
 
-
+//函数
 //func add (x int, y int) int {
 //	return x + y
 //}
@@ -24,6 +35,17 @@ package main
 //}
 
 
+//函数 续 当连续两个或多个函数的已命名形参类型相同时，除最后一个类型以外，其它都可以省略
+//func add (x, y int) int {
+//	return x + y
+//}
+//
+//func main() {
+//	fmt.Println(add(12,  30))
+//}
+
+
+//多值返回
 //func swap(x, y string) (string, string) {
 //	return x, y
 //}
@@ -36,6 +58,7 @@ package main
 //}
 
 
+//命名返回值
 //func split(sum int) (x, y int){
 //	x = sum * 4 / 9
 //	y = sum - x
@@ -53,6 +76,7 @@ package main
 //}
 
 
+//变量
 //var c, python, java bool
 //
 //func main() {
@@ -61,6 +85,7 @@ package main
 //}
 
 
+//变量初始化
 //var i, j int = 1, 2
 //
 //func main() {
@@ -68,7 +93,7 @@ package main
 //	fmt.Println(i, j, c, python, java)
 //}
 
-
+//短变量声明
 //func main() {
 //	var i, j int = 1, 2
 //	k := 3
@@ -77,6 +102,7 @@ package main
 //}
 
 
+//基本类型
 //var (
 //	ToBe bool = false
 //	MaxInt uint64 = 1<<64 - 1
@@ -90,6 +116,10 @@ package main
 //}
 
 
+//零值 没有明确初始值的变量声明会被赋予它们的 零值。
+//数值类型为 0，
+//布尔类型为 false，
+//字符串为 ""（空字符串）。
 //func main() {
 //	var i int
 //	var f float64
@@ -99,6 +129,7 @@ package main
 //}
 
 
+//类型转换
 //func main() {
 //	var x, y int = 3, 4
 //	var f float64 = math.Sqrt(float64(x*x + y*y))
@@ -107,12 +138,14 @@ package main
 //}
 
 
+//类型推导
 //func main() {
 //	v := 42 // 修改这里！
 //	fmt.Printf("v is of type %T\n", v)
 //}
 
 
+//常量
 //const Pi = 3.14
 //
 //func main() {
@@ -122,4 +155,27 @@ package main
 //
 //	const Truth = true
 //	fmt.Println("Go rules?", Truth)
+//}
+
+
+//数值常量
+//const (
+//	Big = 1 << 100
+//	Small = Big >> 99
+//)
+//
+//func needInt(x int) int {
+//	return x * 10 + 1
+//}
+//func needFloat(x float64) float64 {
+//	return x * 0.1
+//}
+//
+//func main() {
+//	fmt.Println(Small)
+//	var BigValue float64 = Big
+//	fmt.Println(BigValue)
+//	fmt.Println(needInt(Small))
+//	fmt.Println(needFloat(Small))
+//	fmt.Println(needFloat(Big))
 //}
